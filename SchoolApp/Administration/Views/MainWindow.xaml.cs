4646be0c577e8eaa10b67f4150ec074eebca06c0
mxn46170 <47306179+mxn46170@users.noreply.github.com>
2019-09-03 21:24:48 -0500

    Add files via upload

---
+++ SchoolApp/Administration/Views/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Administration.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Administration
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ShutDown(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+            //check if the current screen has any unsaved data -> output message to save or not?
+        }
+
+        private void DisplaySchoolSetupView(object sender, RoutedEventArgs e)
+        {
+            //Temporary code, this is not how we want to do this
+            tmp.Visibility = Visibility.Visible;
+        }
+
+        private void OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            // Begin dragging the window
+            DragMove();
+        }
+    }
+}
