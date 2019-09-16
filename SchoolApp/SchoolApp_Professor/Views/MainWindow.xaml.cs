363c5a8d02737c012a22b1da53ca04431b7eb43c
Sugam Shrestha <sugamsh23@gmail.com>
2019-09-16 14:51:56 -0500

    Add icons and implement shut down functionality

--- SchoolApp/SchoolApp_Professor/Views/MainWindow.xaml.cs
+++ SchoolApp/SchoolApp_Professor/Views/MainWindow.xaml.cs
@@ -25,11 +25,22 @@ namespace SchoolApp_Professor
             InitializeComponent();
         }
 
+        private void ShutDown(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+            //check if the current screen has any unsaved data -> output message to save or not?
+        }
+
         private void OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             base.OnMouseLeftButtonDown(e);
             // Begin dragging the window
             DragMove();
+        }
+
+        private void DisplayPersonalInformationView(object sender, RoutedEventArgs e)
+        {
+            tmp.Visibility = Visibility.Visible;
         }
     }
 }
